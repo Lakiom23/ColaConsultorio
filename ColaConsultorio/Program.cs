d2903c374903769cf5e0c0ef4aaa923b64c49c65
Maikolpenia <125204467+Lakiom23@users.noreply.github.com>
2024-03-04 15:07:14 -0400

    Actualizar Program.cs
    función main

--- ColaConsultorio/Program.cs
+++ ColaConsultorio/Program.cs
@@ -7,33 +7,9 @@ namespace ColaConsultorio
 
         static void Main(string[] args)
         {
-            ColaPacientes cola = new ColaPacientes();
-
-            Medico medico = new Medico("Juan",
-                                        "Lopez",
-                                        34,
-                                        336345,
-                                        "pediatria"
-                                    );
-
-
-            Paciente paciente = new Paciente("Pedro",
-                                            "Sanchez",
-                                            3,
-                                            336345,
-                                            1
-                                        );
-
-            medico.PacienteAtendido();
-            medico.AtenderPaciente(paciente);
-            medico.MostrarPaciente();
-            medico.PacienteAtendido();
-
-            cola.Mostrar();
-            cola.Push(paciente);
-            cola.Mostrar();
-            cola.Pop();
-            cola.Mostrar();
+                Hospital consultorio = new Hospital();
+consultorio.Menu();
+Console.ReadKey();
         }
     }
 }
