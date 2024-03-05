d7b7c6487616a1147ff60fa3ca639e69a9d7dd6a
Maikolpenia <125204467+Lakiom23@users.noreply.github.com>
2024-03-05 09:37:54 -0400

    Actualizar ClasePaciente.cs

--- ColaConsultorio/ClasePaciente.cs
+++ ColaConsultorio/ClasePaciente.cs
@@ -15,5 +15,19 @@ namespace ColaConsultorio
             get { return prioridad; }
             set { prioridad = value; }
         }
+        
+                public void MostrarDatos()
+        {
+            
+                    Console.WriteLine($"--------------------------------------");
+                    Console.WriteLine($"Nombre : {Nombre} ");
+                    Console.WriteLine($"Apellido : {Apellido} ");
+                    Console.WriteLine($"Edad : {Edad} ");
+                    Console.WriteLine($"Cedula : {Cedula} ");
+                    Console.WriteLine($"--------------------------------------");
+                    
+        }
+
+
     }
 }
