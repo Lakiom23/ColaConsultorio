80fe422d2b2865541f6fe89459728dfdc2e10a56
Maikolpenia <125204467+Lakiom23@users.noreply.github.com>
2024-03-06 16:19:56 -0400

    Actualizar ClaseHospital.cs

--- ColaConsultorio/ClaseHospital.cs
+++ ColaConsultorio/ClaseHospital.cs
@@ -5,7 +5,7 @@ namespace ColaConsultorio
     //Class Hospital
     public class Hospital
     {
-       /* private ColaPacientes[]? adultos;
+               /* private ColaPacientes[]? adultos;
         private ColaPacientes[]? ninios;
         private Medico? general;
         private Medico? pediatra;*/
@@ -16,21 +16,31 @@ namespace ColaConsultorio
         //Constructor
         public Hospital()
         {
-               colaPacientes = new ColaPacientes[2, 5];
-                medico = new Medico[2];
-                medico[0] =  new Medico("Lenin", "Figeroa", 23, 23215546, "General");
-                medico[1] = new Medico("Maikol", "Penia", 23, 27275746, "Pediatria");
+            colaPacientes = new ColaPacientes[2, 5];
+            for(int i = 0 ; i< 2; i++){
+                for (int j = 0; j < 5; j++)
+                {
+                    colaPacientes[i,j] = new ColaPacientes();
 
-           /* general = new Medico("Lenin", "Figueroa", 23, 23215546, "General");
-            pediatra = new Medico("Maikol", "Penia", 23, 27275746, "Pediatria");
-            adultos = new ColaPacientes[5];
-            ninios = new ColaPacientes[5];
+                }
 
-            for (int i = 0; i < 5; i++)
-            {
-                adultos[i] = new ColaPacientes();
-                ninios[i] = new ColaPacientes();
-            }*/
+            }
+
+
+            medico = new Medico[2];
+            medico[0] = new Medico("Lenin", "Figeroa", 23, 23215546, "General");
+            medico[1] = new Medico("Maikol", "Penia", 23, 27275746, "Pediatria");
+
+            /* general = new Medico("Lenin", "Figueroa", 23, 23215546, "General");
+             pediatra = new Medico("Maikol", "Penia", 23, 27275746, "Pediatria");
+             adultos = new ColaPacientes[5];
+             ninios = new ColaPacientes[5];
+
+             for (int i = 0; i < 5; i++)
+             {
+                 adultos[i] = new ColaPacientes();
+                 ninios[i] = new ColaPacientes();
+             }*/
 
         }
 
@@ -40,9 +50,10 @@ namespace ColaConsultorio
         internal ColaPacientes[] Adultos { get => adultos; set => adultos = value; }
         internal ColaPacientes[] Ninios { get => ninios; set => ninios = value; }*/
 
-         internal Medico[] Medico { get => medico; set => medico = value; }
-          internal ColaPacientes[,] ColaPacientes { get => colaPacientes; set => colaPacientes = value; }
-            
+        internal Medico[] Medico { get => medico; set => medico = value; }
+        internal ColaPacientes[,] ColaPacientes{ get => colaPacientes; set => colaPacientes = value;}
+
+
 
 
 
@@ -67,15 +78,17 @@ namespace ColaConsultorio
 
             Paciente pacienteNuevo = new Paciente(nombre, apellido, edad, cedula);
 
+            Console.WriteLine($"El paciente Necesita ir a Cola de {Caso(pacienteNuevo.Prioridad)}");
+
             if (pacienteNuevo.Edad <= 15)
             {
                 //ninios[pacienteNuevo.Prioridad].Push(pacienteNuevo);
-                colaPacientes[0,pacienteNuevo.Prioridad].Push(pacienteNuevo);
+                colaPacientes[0, pacienteNuevo.Prioridad].Push(pacienteNuevo);
             }
             else
             {
                 //adultos[pacienteNuevo.Prioridad].Push(pacienteNuevo);
-                colaPacientes[1,pacienteNuevo.Prioridad].Push(pacienteNuevo);
+                colaPacientes[1, pacienteNuevo.Prioridad].Push(pacienteNuevo);
             }
         }
 
@@ -83,19 +96,20 @@ namespace ColaConsultorio
         public void AtenderPaciente()
         {
             medico[0].PacienteAtendido();
-             medico[1].PacienteAtendido();
-            
-for (int i = 0;i < 2 ; i++)
-                {
-                    for (int j = 0; medico[i].Paciente == null || j < 5; i++)
-                        medico[i].AtenderPaciente(colaPacientes[i,j].Pop());
-                }
+            medico[1].PacienteAtendido();
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; medico[i].Paciente == null || j < 5; j++) {
+                    medico[i].AtenderPaciente(colaPacientes[i, j].Pop()); }
+
+            }
 
             /*
             general.PacienteAtendido();
             pediatra.PacienteAtendido();
 
-          
 
             for (int i = 0; general.Paciente == null || i < 5; i++)
             {
@@ -132,45 +146,48 @@ for (int i = 0;i < 2 ; i++)
         public void GenerarReporte()
         {
             Console.WriteLine("REPORTE MEDICO ACTUAL");
-            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||");
-            Console.WriteLine("Medico General");
+            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||\n");
+            Console.WriteLine("Medico Pediatra");
             //general.MostrarPaciente();
             medico[0].MostrarPaciente();
-            Console.WriteLine($"--------------------------------------");
-            Console.WriteLine("Medico Pediatra");
+            Console.WriteLine($"--------------------------------------\n ");
+            Console.WriteLine("Medico General ");
             //pediatra.MostrarPaciente();
             medico[1].MostrarPaciente();
-            Console.WriteLine($"||||||||||||||||||||||||||||||||||||||");
+            Console.WriteLine($"|||||||||||||||||||||||||||||||||||||| \n");
             ColaPacientes colaAuxiliar = new ColaPacientes();
             Paciente pacienteAuxiliar;
 
-            for(int i = 0; i < 2; i++)
+            for (int i = 0; i < 2; i++)
+            {
+                if(i == 0) Console.WriteLine("COLAS DE NIÑOS \n");
+                else Console.WriteLine("COLAS DE ADULTOS \n ");
+
+                for (int j = 0; j < 5; j++)
                 {
-                    for (int j = 0; j < 5; j++)
+                    if (colaPacientes[i, j].Vacia())
                     {
-                        if (colaPacientes[i, j].Vacia())
-                        {
-                            Console.WriteLine($"Vacia la cola de {Caso(j)}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Pacientes en cola de {Caso(j)}");
-                            while (!colaPacientes[i, j].Vacia())
-                            {
-                                pacienteAuxiliar = colaPacientes[i, j].Pop();
-                                pacienteAuxiliar.MostrarDatos();
-                                colaAuxiliar.Push(pacienteAuxiliar);
-                            }
-                            while (colaAuxiliar.Vacia())
-                            {
-                                colaPacientes[i, j].Push(colaAuxiliar.Pop());
-                            }
-                        }
-
+                        Console.WriteLine($"Vacia la cola de {Caso(j)}");
                     }
-                }
+                    else
+                    {
+                        Console.WriteLine($"Pacientes en cola de {Caso(j)}");
+                        while (!colaPacientes[i, j].Vacia())
+                        {
+                            pacienteAuxiliar = colaPacientes[i, j].Pop();
+                            pacienteAuxiliar.MostrarDatos();
+                            colaAuxiliar.Push(pacienteAuxiliar);
+                        }
+                        while (!colaAuxiliar.Vacia())
+                        {
+                            colaPacientes[i, j].Push(colaAuxiliar.Pop());
+                        }
+                    }
 
-            
+                }
+            }
+
+
 
             /*
 
@@ -237,25 +254,31 @@ for (int i = 0;i < 2 ; i++)
                 Console.WriteLine("2.Atender Pacientes");
                 Console.WriteLine("3.Generar reporte ");
                 Console.WriteLine("4.CERRAR SISTEMA ");
+                Console.Write("Ingrese una Opcion por favor : ");
 
                 opcion = Console.ReadLine();
-
+                Console.Clear();
                 switch (opcion)
                 {
                     case "1":
                         InsertarPaciente();
                         Console.ReadKey();
+                        Console.Clear();
                         break;
 
                     case "2":
                         AtenderPaciente();
                         Console.ReadKey();
+                        Console.Clear();
                         break;
+
                     case "3":
                         GenerarReporte();
                         Console.ReadKey();
+                        Console.Clear();
                         break;
                     default:
+                        Console.WriteLine("Por Favor ingrese una Opcion validad");
                         break;
                 }
 
